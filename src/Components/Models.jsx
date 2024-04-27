ac0879b01760048db66d86579c4a4a2b8856b5d1
developercris7 <developercris777@gmail.com>
2024-04-27 17:27:39 +0530

    tesla homepage created

--- src/Components/Models.jsx
+++ src/Components/Models.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Styles from "../Assets/css/models.module.css";
 
-const Models = () => {
+const Models = ({ model }) => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  let imageUrl;
+  if (windowWidth < 768) {
+    // Mobile
+    imageUrl = model.backgroundmobile;
+  } else if (windowWidth < 1024) {
+    // Tablet
+    imageUrl = model.backgroundtab;
+  } else {
+    // Desktop
+    imageUrl = model.background;
+  }
+
   return (
-    <div>Models</div>
-  )
-}
+    <main style={{ backgroundImage: `url(${imageUrl})` }}>
+      <div className={Styles.modelTextSection}>
+        <div className={Styles.textHead}>
+          <h1>{model.name}</h1>
+          <h5>{model.price}</h5>
+          <p>{model.credit}</p>
+        </div>
+        <div  className={Styles.textFooter}>
+          <div  className={Styles.modelsButtonsSection}>
+            <button className={Styles.viewButton}>View Model</button>
+            <button className={Styles.demoButton}>Demo Drive</button>
+          </div>
+          <div>
+          <p className="m-0 fw px-3" style={{color:`${model.creditTextColor}`}} >{model.creditText}</p>
+          <a href="/" className="m-0 fw"  style={{color:`${model.creditTextColor}`}} >{model.anchorText}</a>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default Models
+export default Models;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-
 import Navbar from '../Components/Navbar'
+import Models from '../Components/Models';
+import data from '../Assets/db.json'
 
 const Home = () => {
- 
+ const arr = data.models;
   return (
     <div>
         <Navbar />
+     {arr.map((model)=> ( <Models model={model}/>))}
     </div>
   )
 }
